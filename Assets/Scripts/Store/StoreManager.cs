5e3644c7aab023fd4642b06e7f38ab2b32df5e32
FrowntownPitt <ajf93@pitt.edu>
2018-12-26 15:46:10 -0500

    Add dynamic store configuration

---
+++ Assets/Scripts/Store/StoreManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Store.Tile;
+
+namespace Store
+{
+    public class StoreManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private int width;
+        [SerializeField]
+        private int height;
+
+        [SerializeField]
+        private TileManager[,] tiles;
+
+        [SerializeField]
+        private GameObject tileContainer;
+
+        public TileManager tilePrefab;
+
+        public bool regenerateFloor = false;
+
+        void Start()
+        {
+            GenerateFloorplan();
+        }
+
+        void Update()
+        {
+            if (regenerateFloor)
+            {
+                regenerateFloor = false;
+                DestroyFloorplan();
+                GenerateFloorplan();
+            }
+        }
+
+        private void DestroyFloorplan()
+        {
+            for (int x = 0; x < tiles.GetLength(1); x++)
+            {
+                for (int y = 0; y < tiles.GetLength(0); y++)
+                {
+                    Destroy(tiles[y, x].gameObject);
+                }
+            }
+        }
+
+        private void GenerateFloorplan()
+        {
+            tiles = new TileManager[height, width];
+            GenerateTiles();
+
+            EnableExteriorWalls();
+        }
+
+        private void EnableExteriorWalls()
+        {
+            for(int x=0; x<width; x++)
+            {
+                tiles[0, x].EnableWall(TileManager.Walls.NORTH);
+                tiles[height - 1, x].EnableWall(TileManager.Walls.SOUTH);
+            }
+
+            for (int y = 0; y < height; y++)
+            {
+                tiles[y, 0].EnableWall(TileManager.Walls.WEST);
+                tiles[y, width-1].EnableWall(TileManager.Walls.EAST);
+            }
+        }
+
+        private void GenerateTiles()
+        {
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Quaternion rotation = Quaternion.identity;
+                    Vector3 position = new Vector3(tilePrefab.GetSizeZ() * y, 0, tilePrefab.GetSizeX() * x);
+                    GameObject newTile = Instantiate(tilePrefab.gameObject, position, rotation, tileContainer.transform);
+                    tiles[y, x] = newTile.GetComponent<TileManager>();
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Store/Tile/TileManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Store.Tile
+{
+    public class TileManager : MonoBehaviour
+    {
+
+        [SerializeField]
+        private GameObject wallContainer;
+
+        [SerializeField]
+        private GameObject northWall;
+        [SerializeField]
+        private GameObject southWall;
+        [SerializeField]
+        private GameObject eastWall;
+        [SerializeField]
+        private GameObject westWall;
+
+        [SerializeField]
+        private float sizeX;
+        [SerializeField]
+        private float sizeZ;
+
+        public enum Walls
+        {
+            NORTH,
+            SOUTH,
+            EAST,
+            WEST
+        }
+
+        void Start()
+        {
+            northWall = wallContainer.transform.Find("North").gameObject;
+            southWall = wallContainer.transform.Find("South").gameObject;
+            eastWall = wallContainer.transform.Find("East").gameObject;
+            westWall = wallContainer.transform.Find("West").gameObject;
+        }
+
+        public void EnableWall(Walls wall)
+        {
+            switch (wall)
+            {
+                case Walls.NORTH:
+                    northWall.SetActive(true);
+                    break;
+                case Walls.SOUTH:
+                    southWall.SetActive(true);
+                    break;
+                case Walls.EAST:
+                    eastWall.SetActive(true);
+                    break;
+                case Walls.WEST:
+                    westWall.SetActive(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public float GetSizeX()
+        {
+            return sizeX;
+        }
+
+        public float GetSizeZ()
+        {
+            return sizeZ;
+        }
+    }
+}
